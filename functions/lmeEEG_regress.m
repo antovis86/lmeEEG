bad1bb638457a6f482847543e9b909fb89abdb58
Antonino Visalli <50337337+antovis86@users.noreply.github.com>
2025-02-28 14:20:30 +0100

    Add files via upload
    Thanks to @johnPGrogan for fixing the bug and improving performance.

--- functions/lmeEEG_regress.m
+++ functions/lmeEEG_regress.m
@@ -1,5 +1,5 @@
 function [tval, b, se] = lmeEEG_regress(y,X)
-% adapted from:
+
 % REGRESS Multiple linear regression using least squares.
 %   References:
 %      [1] Chatterjee, S. and A.S. Hadi (1986) "Influential Observations,
@@ -9,8 +9,7 @@ function [tval, b, se] = lmeEEG_regress(y,X)
 %          ed., Wiley.
 %   Copyright 1993-2014 The MathWorks, Inc.
 
-
-
+nY = size(y,2); % number of columns of Y passed in - e.g. time-samples
 
 [n,ncolX] = size(X);
 % Use the rank-revealing QR to remove dependent columns of X.
@@ -31,24 +30,25 @@ end
 
 % Compute the LS coefficients, filling in zeros in elements corresponding
 % to rows of X that were thrown out.
-b = zeros(ncolX,1);
-b(perm) = R \ (Q'*y);
+% Column of coefficients per column in Y
+b = zeros(ncolX,nY);
+b(perm,:) = R \ (Q'*y);
 
 
 % compute SE e tval
 RI = R\eye(p);
-nu = max(0,n-p);                % Residual degrees of freedom
+nu = max(0,n-p);                % Residual degrees of freedom, same for all columns
 yhat = X*b;                     % Predicted responses at each data point.
 r = y-yhat;                     % Residuals.
-normr = norm(r);
+normr = vecnorm(r);             % get norm per column
 if nu ~= 0
     rmse = normr/sqrt(nu);      % Root mean square error.
 else
     rmse = NaN;
 end
 
-se = zeros(ncolX,1);
-se(perm,:) = rmse*sqrt(sum(abs(RI).^2,2));
+se = zeros(ncolX,nY); % per column
+se(perm,:) = rmse .* sqrt(sum(abs(RI).^2,2)); % per column
 tval = b./se;
 
 
